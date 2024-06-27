74dd0dd604f103ca22ee8e9a2a4b1e47924a45eb
Facundo Cánepa <facundo.villalba.canepa@gmail.com>
2024-06-27 20:13:28 -0300

    agregola funcion sumar y restar

---
+++ ejemplo2.py
@@ -0,0 +1,5 @@
+def sumar (n1,n2):
+        print("el resultado de la sma entre n1 ´n2 es de: ", n1 + n2)
+
+sumar(int(input("Ingresa el primer numero: ")),int(input("Ingresa el segundo numero: ")))
+    
